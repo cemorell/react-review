d9168d5c8b62f41a3dd2c349352de2860afc12d3
camille morell <cemorell>
2016-07-20 18:57:40 -0500

    DONE BEFORE 7

---
+++ starter_code/components.js
@@ -0,0 +1,50 @@
+
+class Image extends React.Component {
+
+ constructor(){
+      super();
+      this.state = {image: "http://www.popoptiq.com/wp-content/uploads/2015/03/marshmallow-man.jpg"};
+  }
+
+  _showImage(newBook){
+    let currentImage = this.state.image;
+
+  if (currentImage === "http://www.popoptiq.com/wp-content/uploads/2015/03/marshmallow-man.jpg"){
+    this.setState({image: ""});
+} else
+{
+      this.setState({image: "http://www.popoptiq.com/wp-content/uploads/2015/03/marshmallow-man.jpg"});
+}
+}
+  render(){
+    return (
+      <div>
+        <h1>Here is an image you don't care about!</h1>
+        <img src={ this.state.image } />
+        <Button click={ this._showImage.bind(this) } />
+      </div>
+    )
+  }
+}
+
+
+class Button extends React.Component {
+    _handleClick(){
+      this.props.click();
+    }
+    render(){
+      return (
+        <div>
+          <button onClick={ this. _handleClick.bind(this) } > REWIND </button>
+        </div>
+        );
+     }
+};
+
+
+
+
+
+
+
+ReactDOM.render(<Image />, document.getElementById('app-container'));
